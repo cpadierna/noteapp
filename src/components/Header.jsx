0d80da5dfa8920992f69a6005547ce34b7b5611e
Christian Padierna <117613662+cpadierna@users.noreply.github.com>
2023-02-02 11:32:51 -0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateIcon from '@mui/icons-material/Create';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CreateIcon />
+        &nbsp; Note-It
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
